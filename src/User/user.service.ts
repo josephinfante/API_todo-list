ad9534467796aca496159c43db5b3bfbc21e0685
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-15 18:00:38 -0500

    Creating query process to consult the user info in mongoDB

--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -1,34 +1,53 @@
-import { Collection, Db } from "mongodb";
+import { Collection, Db, ObjectId } from "mongodb";
 import { User } from "../models/user.model";
-import { closeConnection, database, openConnection } from "../utils/connection.utils";
+import {
+  closeConnection,
+  database,
+  openConnection,
+} from "../utils/connection.utils";
 
 export class UserService {
-    async createUser(user: User) {
-        try {
-            await openConnection();
-            let db: Db = await database();
-            let collection: Collection = db.collection("User")
-            let newUser: User = {
-                name: user.name,
-                lastname: user.lastname,
-                email: user.email,
-                password: user.password,
-                created_at: new Date(),
-                updated_at: new Date(),
-                tasks: [],
-            }
-            await collection.insertOne(newUser);
-            await closeConnection();
-            return newUser
-        } catch (error) {
-            await closeConnection();
-            return error
-        }
+  async getUser(id: string) {
+    try {
+      await openConnection();
+      let db: Db = await database();
+      let userCollection: Collection = db.collection("User");
+      let result = await userCollection
+        .find()
+        .project({ _id: new ObjectId(id), name: 1, lastname: 1, email: 1 })
+        .toArray();
+      await closeConnection();
+      if (result[0]) {
+        return result[0];
+      }
+      return { error: `User with ID ${id} not found` };
+    } catch (error) {
+      await closeConnection();
+      return { error: `User with ID ${id} not found` };
     }
-    updateUser() {
-
+  }
+  async createUser(user: User) {
+    try {
+      await openConnection();
+      let db: Db = await database();
+      let collection: Collection = db.collection("User");
+      let newUser: User = {
+        name: user.name,
+        lastname: user.lastname,
+        email: user.email,
+        password: user.password,
+        created_at: new Date(),
+        updated_at: new Date(),
+        tasks: [],
+      };
+      await collection.insertOne(newUser);
+      await closeConnection();
+      return { message: "User account created" };
+    } catch (error) {
+      await closeConnection();
+      return error;
     }
-    deleteUser() {
-
-    }
+  }
+  updateUser() {}
+  deleteUser() {}
 }
