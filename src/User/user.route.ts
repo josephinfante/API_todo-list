63607da5c125eff8db102331ce166db4895d0b40
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-15 18:00:14 -0500

    handling route for when getting the user info by ID

--- src/User/user.route.ts
+++ src/User/user.route.ts
@@ -1,21 +1,26 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { UserService } from "./user.service";
+import { signUp } from './user.middleware'
 
 const userRouter = Router();
-const userService = new UserService()
+const userService = new UserService();
 
 userRouter
-  .get("/:id", (req: Request, res: Response, next: NextFunction) => {
-    req;
-    res.status(200).json({ message: "Getting all users" });
-    next();
-  })
-  .post("/", async (req: Request, res: Response) => {
+  .get("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let user = await userService.createUser(req.body)
+      let user = await userService.getUser(req.params.id);
+      user.error ? res.status(400).send(user) : res.status(200).send(user);
+      next();
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  })
+  .post("/", signUp, async (req: Request, res: Response) => {
+    try {
+      let user = await userService.createUser(req.body);
       res.status(201).send(user);
     } catch (error) {
-      res.status(400).send(error)
+      res.status(400).send(error);
     }
   })
   .put("/:id", (req: Request, res: Response) => {
