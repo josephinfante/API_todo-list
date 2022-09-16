88e70dadb532d0bef509230a3e3747366c3ab260
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-16 17:12:21 -0500

    Adding authentication middleware

--- src/Task/task.route.ts
+++ src/Task/task.route.ts
@@ -1,44 +1,43 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { authenticateToken } from "../User/user.middleware";
 import { TaskService } from "./task.service";
 
 const taskRouter = Router();
 const taskService = new TaskService();
 
 taskRouter
-  .get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  .get(
+    "/:id",
+    authenticateToken,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        let response = await taskService.getTask(req.params.id);
+        res.status(200).send(response);
+      } catch (error) {
+        res.status(400).send(error);
+      }
+      next();
+    }
+  )
+  .post("/", authenticateToken, async (req: Request, res: Response) => {
     try {
-      let task = await taskService.getTask(req.params.id);
-      res.status(200).send(task);
+      let response = await taskService.createTask(req.body, res.locals.id);
+      res.status(201).send(response);
     } catch (error) {
       res.status(400).send(error);
     }
-    next();
   })
-  .post("/:user_id", async (req: Request, res: Response) => {
+  .put("/", authenticateToken, async (req: Request, res: Response) => {
     try {
-      await taskService.createTask(req.body, req.params.user_id);
-      res.status(201).send();
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  })
-  .put("/:user_id", async (req: Request, res: Response) => {
-    try {
-      let response = await taskService.updateTask(
-        req.params.user_id,
-        req.body
-      );
+      let response = await taskService.updateTask(res.locals.id, req.body);
       res.status(200).send(response);
     } catch (error) {
       res.status(400).send(error);
     }
   })
-  .delete("/user_id/:id", async (req: Request, res: Response) => {
+  .delete("/:id", authenticateToken, async (req: Request, res: Response) => {
     try {
-      let response = await taskService.deleteTask(
-        req.params.user_id,
-        req.params.id
-      );
+      let response = await taskService.deleteTask(res.locals.id, req.params.id);
       res.status(200).send(response);
     } catch (error) {
       res.status(400).send(error);
