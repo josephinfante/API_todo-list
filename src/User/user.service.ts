0522713ff25ffa097e280a71711b0b7cb558f2b7
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-16 17:14:19 -0500

    Handling password encryptation, and generating a token for the user

--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -5,6 +5,9 @@ import {
   database,
   openConnection,
 } from "../utils/connection.utils";
+import { encrypt, decrypt } from "../utils/hash.utils";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 export class UserService {
   async getUser(id: string) {
@@ -23,31 +26,105 @@ export class UserService {
       return { error: `User with ID ${id} not found` };
     } catch (error) {
       await closeConnection();
-      return { error: `User with ID ${id} not found` };
+      return { error: "There is an error getting the user, try again later" };
     }
   }
   async createUser(user: User) {
     try {
+      let hashedPassword = await encrypt(user.password);
       await openConnection();
       let db: Db = await database();
-      let collection: Collection = db.collection("User");
+      let userCollection: Collection = db.collection("User");
       let newUser: User = {
         name: user.name,
         lastname: user.lastname,
         email: user.email,
-        password: user.password,
+        password: hashedPassword,
         created_at: new Date(),
         updated_at: new Date(),
         tasks: [],
       };
-      await collection.insertOne(newUser);
+      await userCollection.insertOne(newUser);
       await closeConnection();
       return { message: "User account created" };
     } catch (error) {
       await closeConnection();
-      return error;
+      return { error: "There is an error creating a user, try again later" };
     }
   }
-  updateUser() {}
-  deleteUser() {}
+  async updateUser(id: string, user: User) {
+    try {
+      await openConnection();
+      let db: Db = await database();
+      let userCollection: Collection = db.collection("User");
+      await userCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $set: {
+            name: user.name,
+            lastname: user.lastname,
+            updated_at: new Date(),
+          },
+        }
+      );
+      await closeConnection();
+      return { message: "User updated" };
+    } catch (error) {
+      await closeConnection();
+      return { error: "There is an error updating the user, try again later" };
+    }
+  }
+  async deleteUser(id: string) {
+    try {
+      //check if the user ID exists
+      let search = await this.getUser(id);
+
+      //If user with that ID don't exists, we return an error
+      if (search.error) {
+        return search;
+      }
+
+      //If user was found, we proceed to remove it
+      await openConnection();
+      let db: Db = await database();
+      let userCollection: Collection = db.collection("User");
+      await userCollection.deleteOne({ _id: new ObjectId(id) });
+      await closeConnection();
+      return { message: "User deleted!" };
+    } catch (error) {
+      await closeConnection();
+      return { error: "There is an error deleting the user, try again later" };
+    }
+  }
+  async login(login: { email: string; password: string }) {
+    try {
+      //check if the email is registered
+      await openConnection();
+      let db: Db = await database();
+      let userCollection: Collection = db.collection("User");
+      let result = await userCollection.find({ email: login.email }).toArray();
+      await closeConnection();
+      //send error status if user with that email don't exist
+      if (result[0].length === 0) {
+        return { error: "There isn't an account with that email" };
+      }
+
+      //compare hashed password with the password provided by the user
+      let passwordMatch = await decrypt(login.password, result[0].password);
+      //if the password match we send a token to the user, if it doesn't we send him an error message
+      if (passwordMatch === false) {
+        return { error: "password is incorrect" };
+      }
+      let token = jwt.sign(
+        result[0]._id.toString(),
+        process.env.JWT_TOKEN ? process.env.JWT_TOKEN : "1234"
+      );
+      return { token: token };
+    } catch (error) {
+      await closeConnection();
+      return {
+        error: "There is an error login into the account, try again later",
+      };
+    }
+  }
 }
